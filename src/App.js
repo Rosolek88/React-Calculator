313ecd037d7a924c13ce2c5a3db3049923fa7bee
Administrator <administrator@CN-6.local>
2019-09-13 16:15:04 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,42 +2,63 @@ import React, {Component} from 'react';
 import './App.css';
 
 
-class App extends React.Component { 
-  render() 
-  { 
-  return ( 
-  <div className="frame"> 
-  <div class="mainCalc"> 
-  <div className="button-row"> 
-    <Button label={'Clear'}/> 
-    <Button label={'Delete'}/> 
-    <Button label={'.'}/> 
-    <Button label={'/'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'7'}/> 
-    <Button label={'8'}/> 
-    <Button label={'9'}/> 
-    <Button label={'*'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'4'}/> 
-    <Button label={'5'}/> 
-    <Button label={'6'}/> 
-    <Button label={'-'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'1'}/> 
-    <Button label={'2'}/> 
-    <Button label={'3'}/> 
-    <Button label={'+'}/> 
-  </div> 
-  <div className="button-row"> 
-    <Button label={'0'}/> 
-    <Button label={'='}/> 
-  </div> 
-  </div> 
-  </div> 
-  ); 
-} 
-} 
+import { Button } from './Button';
+
+class App extends React.Component {
+  state = {
+    count: [],
+    keys: [
+      { value: "clear", styleName: "clearButton" },
+      { value: "/", styleName: "operatorButton" },
+      { value: "7", styleName: "numberButton" },
+      { value: "8", styleName: "numberButton" },
+      { value: "9", styleName: "numberButton" },
+      { value: "*", styleName: "operatorButton" },
+      { value: "4", styleName: "numberButton" },
+      { value: "5", styleName: "numberButton" },
+      { value: "6", styleName: "numberButton" },
+      { value: "-", styleName: "operatorButton" },
+      { value: "1", styleName: "numberButton" },
+      { value: "2", styleName: "numberButton" },
+      { value: "3", styleName: "numberButton" },
+      { value: "+", styleName: "operatorButton" },
+      { value: "0", styleName: "numberButton zero"  },
+      { value: "=", styleName: "operatorButton" }
+    ]
+  }
+
+  handleButton = (value) => {
+    if (value === "clear") {
+      this.setState({ count: [] })
+    } else if (value === "=") {
+      this.setState(prevState => ({
+        count: [eval(prevState.count.join(""))]
+      }))
+    } else {
+      this.setState(prevState => ({
+        count: [...prevState.count, value]
+      }))
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="calc-body">
+          <div className="value-row">
+            <h1 className="value-box" placeholder="0"> {this.state.count} </h1>
+          </div>
+          <div className="buttonsWrapper">
+            <div className="buttons">
+              {this.state.keys.map(key => {
+                return <Button handleButton={this.handleButton} data={key} />
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
